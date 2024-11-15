283adbd0aa7bc67f9b2144a8e91ecd1334d4e791
崔志伟 <xiaowei5486@126.com>
2024-11-15 17:08:20 +0800

    添加KeepAliveWidget

--- lib/lj_flutter_package.dart
+++ lib/lj_flutter_package.dart
@@ -1,12 +1,16 @@
 library lj_flutter_package;
 
+export 'ui_component/lj_network_image.dart';
+
 export 'utils/lj_define.dart';
 export 'utils/lj_event_bus.dart';
 export 'utils/lj_extensions.dart';
 export 'utils/lj_network.dart';
-export 'ui_component/lj_network_image.dart';
 export 'utils/lj_permission.dart';
 export 'utils/lj_router_manager.dart';
 export 'utils/lj_router_manager_get.dart';
 export 'utils/lj_util.dart';
+
+export 'widgets/keep_alive_widget.dart';
+
 export 'package:get/get.dart';
---
+++ lib/widgets/keep_alive_widget.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/widgets.dart';
+
+class KeepAliveWidget extends StatefulWidget {
+  final Widget child;
+
+  const KeepAliveWidget({super.key, required this.child});
+
+  @override
+  State<KeepAliveWidget> createState() => _KeepAliveWidgetState();
+}
+
+class _KeepAliveWidgetState extends State<KeepAliveWidget>
+    with AutomaticKeepAliveClientMixin {
+  @override
+  Widget build(BuildContext context) {
+    return widget.child;
+  }
+
+  @override
+  bool get wantKeepAlive => true;
+}
