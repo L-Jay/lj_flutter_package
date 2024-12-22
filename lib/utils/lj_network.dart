c3f4d44a875b284b9a8ebfd9d3c1303b54518f31
崔志伟 <xiaowei5486@126.com>
2024-12-22 15:12:15 +0800

    LJNetwork参数拦截handleRequestParams优化

--- lib/utils/lj_network.dart
+++ lib/utils/lj_network.dart
@@ -273,7 +273,7 @@ class LJNetwork {
         }
 
         /*拦截请求参数*/
-        if (handleRequestParams != null && data is Map<String, dynamic>) {
+        if (handleRequestParams != null) {
           data = handleRequestParams!(path, data);
         }
       }
