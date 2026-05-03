fd51f102919c60a1ead6fec68e4d4155659d1e5d
崔志伟 <xiaowei5486@126.com>
2026-05-03 11:29:38 +0800

    1.升级视频 2.支付支付使用tobias

--- lib/utils/lj_alipay_util.dart
+++ lib/utils/lj_alipay_util.dart
@@ -1,35 +1,13 @@
 import 'dart:async';
 
-import 'package:alipay_kit/alipay_kit.dart';
+import 'package:tobias/tobias.dart';
 
-import 'lj_error.dart';
-
-///所有方法只有成功才返回
-///所有错误使用register的errorCallback处理
 class AlipayUtil {
-  static final instance = AlipayKitPlatform.instance;
+  static final Tobias tobias = Tobias();
 
-  static Future<bool> get installed => instance.isInstalled();
-  static Completer _completer = Completer();
-
-  static register({
-    required Function(LJError error) errorCallback,
-  }) async {
-    instance.payResp().listen((AlipayResp resp) {
-      if (resp.resultStatus == 9000) {
-        _completer.complete();
-      } else {
-        errorCallback(LJError(
-          resp.resultStatus ?? 600,
-          resp.memo?.isNotEmpty == true ? resp.memo! : '支付失败',
-        ));
-      }
-    });
-  }
+  static Future<bool> get installed => tobias.isAliPayInstalled;
 
   static Future pay(String order) {
-    _completer = Completer();
-    instance.pay(orderInfo: order);
-    return _completer.future;
+    return tobias.pay(order);
   }
 }
